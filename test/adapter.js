2ca964949c6ab1ebaceaf674bb64acab31b983e0
Ruben Verborgh <ruben@verborgh.org>
2013-12-15 14:53:08 +0000

    Upgrade to 2.0 version of tests.

--- test/adapter.js
+++ test/adapter.js
@@ -1,13 +1,6 @@
 var promiscuous = require('../promiscuous');
 module.exports = {
-  fullfilled: promiscuous.resolve,
-  rejected:   promiscuous.reject,
-  pending: function () {
-    var deferred = promiscuous.deferred();
-    return {
-      promise: deferred.promise,
-      fulfill: deferred.resolve,
-      reject:  deferred.reject,
-    };
-  },
+  resolved: promiscuous.resolve,
+  rejected: promiscuous.reject,
+  deferred: promiscuous.deferred,
 };
