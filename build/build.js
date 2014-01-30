d969c88ebca8300c8cdbcec1d19df7b37093e7f0
Ruben Verborgh <ruben@verborgh.org>
2014-01-30 15:10:25 +0000

    Also build unminified browser version.
    Addresses #21.

--- build/build.js
+++ build/build.js
@@ -2,16 +2,20 @@
 var fs = require("fs");
 var UglifyJS = require("uglify-js");
 
-var copyright = fs.readFileSync("promiscuous.js", "utf8").match(/.*\n/)[0];
-var minified = copyright + UglifyJS.minify("promiscuous.js").code;
-var browserified = minified.replace("module.exports", "Promise")
-                           .replace("setImmediate", "setTimeout");
+var full = fs.readFileSync("promiscuous.js", "utf8");
+var copyright = full.match(/.*\n/)[0];
+var minified = copyright + UglifyJS.minify(full, { fromString: true }).code;
+var browserFull = full.replace("module.exports", "window.Promise")
+                      .replace("setImmediate", "setTimeout");
+var browser = copyright + UglifyJS.minify(browserFull, { fromString: true }).code
+                                  .replace("window.Promise", "Promise");
 
 var path = "dist/";
 if(!fs.existsSync(path))
   fs.mkdirSync(path);
 
 fs.writeFileSync(path + "promiscuous-node.js", minified);
-fs.writeFileSync(path + "promiscuous-browser.js", browserified);
+fs.writeFileSync(path + "promiscuous-browser.js", browser);
+fs.writeFileSync(path + "promiscuous-browser-full.js", browserFull);
 fs.writeFileSync(path + "promiscuous-shim.js",
-                 browserified.replace("!", "(typeof Promise)[0]!='u'||"));
+                 browser.replace("!", "(typeof Promise)[0]!='u'||"));
